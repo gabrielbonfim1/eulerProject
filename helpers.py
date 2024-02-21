0fd96d2f6387fd366e8fb791288505c4fbec5a96
Gabriel Bonfim <gabriel.zip@hotmail.com>
2024-02-21 10:05:03 -0300

    PrimeSieve

--- helpers.py
+++ helpers.py
@@ -16,6 +16,25 @@ def genPrimes():
             yield x
             x += 1
 
+    
+def primeSieve(num):
+    prime = [True for i in range(num)]
+    p = 2
+    while (p * p <= num): 
+        # If prime[p] is not
+        # changed, then it is a prime
+        if (prime[p] == True): 
+            # Updating all multiples of p
+            for i in range(p * p, num, p):
+                prime[i] = False
+        p += 1
+ 
+    # Print all prime numbers
+    for p in range(2, num+1):
+        if prime[p]:
+            return [ind for ind,p in enumerate(prime) if p and ind > 1]
+    
+
 def IsPalindrome(num):
     num_string = str(int(num))
     return num_string == num_string[::-1]
