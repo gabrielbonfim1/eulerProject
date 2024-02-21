5382ba87f2318cff822a660387e464e8a8a297d6
Gabriel Bonfim <gabriel.zip@hotmail.com>
2024-02-21 08:39:14 -0300

    prime generator

--- helpers.py
+++ helpers.py
@@ -1,2 +1,16 @@
-def print1():
-    print(1)
+def genPrimes():
+    primes = [2]
+    x = 2
+    while True:        
+        if x == 2:
+            yield 2
+            x += 1
+        
+        for prime in primes:
+            if x % prime == 0:
+                x += 1
+                break
+        else:
+            primes.append(prime)
+            yield x
+            x += 1
