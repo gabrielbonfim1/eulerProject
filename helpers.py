3c84f7e805c7fe7ad76ca208a4d9e412c4079a28
Gabriel Bonfim <gabriel.zip@hotmail.com>
2024-02-22 08:27:23 -0300

    Timeit Function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,19 @@
 from timeit import default_timer as timer
 import math
+from functools import wraps
+import time
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
 
 def genPrimes():
     primes = [2]
